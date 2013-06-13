CoolCold/vzstats

commit 69f94571743f60cd9b7d0442941285e7713f2e41

First attempt to make paths configurable
Not including the man page yet.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

Makefile
@@ -1,11 +1,7 @@
 NAME=vzstats
-ETCDIR=/etc/vz
-SBINDIR=/usr/sbin
-MANDIR=/usr/share/man
-MAN8DIR=$(MANDIR)/man8
-REPDIR=/usr/libexec/$(NAME)
+include Makefile.paths
 
-all:
+all: $(NAME) $(NAME).conf
 .PHONY: all
 
 install:
@@ -37,6 +33,7 @@ install-all: install install-bashcomp install-cronjob
 .PHONY: install-all
 
 clean:
+	rm -f $(NAME) $(NAME).conf
 .PHONY: clean
 
 # Tar and rpm build

.gitignore
@@ -1,1 +1,3 @@
 vzstats*.tar.bz2
+vzstats
+vzstats.conf

Makefile.paths
@@ -1,0 +1,17 @@
+# Configuration file
+ETCDIR=/etc/vz
+
+# Binary
+SBINDIR=/usr/sbin
+
+# Man page
+MANDIR=/usr/share/man
+MAN8DIR=$(MANDIR)/man8
+
+# Report script files
+REPDIR=/usr/libexec/$(NAME)
+
+%: %.in
+	sed -e 's!@ETCDIR@!$(ETCDIR)!g' \
+	    -e 's!@REPDIR@!$(REPDIR)!g' \
+		$< > $@

vzstats.conf.in
@@ -1,4 +1,4 @@
-REP_DIR=/usr/libexec/vzstats
+REP_DIR=@REPDIR@
 URL=http://stats.openvz.org
 SUBMIT_URL=$URL/submit.php
 GET_ID_URL=$URL/genuuid.php

vzstats.in
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-DISABLED_MARK=/etc/vz/vzstats-disable
+DISABLED_MARK=@ETCDIR@/vzstats-disable
 # vzstats disabled and no arguments provided -- exit now
 test -f $DISABLED_MARK -a $# -eq 0 && exit 0
 
-. /etc/vz/vzstats.conf
-IDFILE=/etc/vz/.vzstats-uuid
+. @ETCDIR@/vzstats.conf
+IDFILE=@ETCDIR@/.vzstats-uuid
 VERSION=0.3.2
 CURL='curl -sS --connect-timeout 3'
 OPT_VIEW=

commit 02bef999f88af69d8ae8061c9dbdbb23b6535903

Makefile: fix rpms target
Broken by commit 69f9457

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

Makefile
@@ -39,7 +39,7 @@ clean:
 # Tar and rpm build
 SPEC=$(NAME).spec
 VERSION_SPEC=$(shell awk '/^Version:/{print $$2}' $(SPEC))
-VERSION_FILE=$(shell awk -F = '($$1=="VERSION") {print $$2}' $(NAME))
+VERSION_FILE=$(shell awk -F = '($$1=="VERSION") {print $$2}' $(NAME).in)
 RELEASE=$(shell awk '/^%define rel / {if ($$3 != 1) print "-"$$3}' $(SPEC))
 NAMEVER=$(NAME)-$(VERSION_SPEC)$(RELEASE)
 TARBALL=$(NAMEVER).tar.bz2